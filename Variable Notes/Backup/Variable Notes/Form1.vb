63d58becf58efe104a945a7b82ff90fed3f15f48
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-02-05 09:08:32 -0500

    Add files via upload

---
+++ Variable Notes/Backup/Variable Notes/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'Variables are used to hold a value in a program.
+        'The value of the variable can change over time.
+        'Variables are always given a name and must be declared
+        'before use.
+
+        'Example:
+        Dim length As Double
+        Dim width As Double
+
+        'I use Dim to create a variable
+        'I must provide a name (I choose) and a data type
+
+        'Legal Names For Variables
+        '1.Contain no spaces or punctuations
+        '2.Contain only letters and numbers
+        '3.Must start w/ letter
+        '4.Underscores are ok
+        '5. Can't be reserved (blue) words 
+        'like sub, Double, etc.
+
+
+        'Other types of data
+        '1. Integer (pos/neg whole #s)
+        '2. Double (pos/neg decimal numbers)
+        '3. String (text)
+
+
+
+        'Get length and width from the user
+        'i.e fill our variables with a value
+        length = TextBox1.Text
+        width = TextBox2.Text
+
+
+        'Calculate (+, -, *, /, ^, () ) VB knows the 
+        'order of operations
+        Dim area As Double
+        area = length * width
+
+
+        'Formatting output (pick 1 of the 3)
+        '***********************************
+        'Output the results to the user
+        'w/no formatting
+        Label4.Text = area
+
+        'output formatted to 3 decimal places
+        Label4.Text = Format(area, "##.###")
+
+        'output as currency
+        Label4.Text = Format(area, "Currency")
+
+
+
+        'not the same as area = label4.text
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        'Determine if the checkbox is checked
+        If CheckBox1.Checked = True Then
+            Button2.Text = "YES"
+        End If
+
+    End Sub
+End Class
---
+++ Variable Notes/Variable Notes/Form1.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form1
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        'Square root
+        Dim x As Integer
+        x = 25
+        Label4.Text = Math.Sqrt(x)
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        'Absolute value
+        Dim x As Integer
+        x = -10
+        Label4.Text = Math.Abs(x)
+    End Sub
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Input Step
+
+        'Variables are used by programs to hold information
+        'Think of variables like little containers
+        'The value of variables can change over time
+
+        'Create variables for the length and width
+        Dim length As Double
+        Dim width As Double
+
+        'Dim (dimension) is what creates a variable
+        'length and width are the names I have given to my variables (my choice)
+        '
+
+        'Legal identifiers for variables
+        '1. Contain no spaces
+        '2. Contain only letters and numbers 
+        '3. Must start with a letter
+        '4. Underscores are fine
+        '5. Can't be a reserved word (in blue)
+
+        'Other types of variables
+        '1. Integer (pos/neg whole number)
+        '2. Double (pos/neg numbers that contain a decimal point)
+        '3. String (text)
+
+
+        'Input from the textboxes
+        length = TextBox1.Text
+        width = TextBox2.Text
+
+        'Compute Area
+        Dim area As Double
+        area = length * width
+        'Math Operations: +, -, *, /, ^, ()
+
+        'Output step
+        'Label4.Text = area
+
+        'Output area formatted to 3 decimal places
+        'Label4.Text = Format(area, "0.000")
+
+        'Output area formatted as currency
+        Label4.Text = Format(area, "currency")
+
+
+
+
+
+
+
+
+    End Sub
+End Class
